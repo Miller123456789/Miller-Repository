66a82668d2254cd0ef5985faeaefaa430ce22c19
Student <student@V103A-ENT-M115.local>
2025-03-06 14:24:38 -0500

    Create SS1_MillerA.js

---
+++ SS1_MillerA/SS1_MillerA.js
@@ -0,0 +1,35 @@
+// Miller A
+// Title: Apple Tree
+// This semester I will just be trying to expand my skill and knowledge of coding,this first sketch is my starting point.
+
+function setup() {
+createCanvas(500, 500);
+
+  background(25,20,250);
+
+  fill(150,75,0);
+  stroke(150,75,0);
+  strokeWeight (4); 
+  rect(250, 350, 25, 70); 
+
+  noStroke()
+  
+  fill('Green')
+  stroke('Green');
+  strokeWeight(3);
+  triangle(175,350,260,100,350,350);
+  fill(0,255,0)
+
+  noStroke()
+
+  fill('Red');
+  stroke('Red');
+  strokeWeight(2);
+  circle(230,250,20);
+  circle(300,315,20);
+  circle(260,180,20);
+  circle(290,230,20);
+  circle(255,290,20);
+  circle(210,320,20);
+
+}
